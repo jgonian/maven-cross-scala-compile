5b0895f4fc89187cc24a2aeae0a93efb0bfeef06
Yannis Gonianakis <ygonianakis@ebay.com>
2016-09-25 12:48:21 +0200

    Keep only the necessary.

--- src/test/scala/samples/scalatest.scala
+++ src/test/scala/samples/scalatest.scala
@@ -15,24 +15,16 @@
  */
 package samples
 
-/*
-ScalaTest facilitates different styles of testing by providing traits you can mix
-together to get the behavior and syntax you prefer.  A few examples are
-included here.  For more information, visit:
-
-http://www.scalatest.org/
-
-One way to use ScalaTest is to help make JUnit or TestNG tests more
-clear and concise. Here's an example:
-*/
 import scala.collection.mutable.Stack
-import org.scalatest.Assertions
+import org.scalatest.{Assertions, Matchers}
 import org.junit.Test
+
+import scala.collection.mutable
 
 class StackSuite extends Assertions {
 
   @Test def stackShouldPopValuesIinLastInFirstOutOrder() {
-    val stack = new Stack[Int]
+    val stack = new mutable.Stack[Int]
     stack.push(1)
     stack.push(2)
     assert(stack.pop() === 2)
@@ -40,23 +32,20 @@ class StackSuite extends Assertions {
   }
 
   @Test def stackShouldThrowNoSuchElementExceptionIfAnEmptyStackIsPopped() {
-    val emptyStack = new Stack[String]
+    val emptyStack = new mutable.Stack[String]
     intercept[NoSuchElementException] {
       emptyStack.pop()
     }
   }
 }
 
-/*
-Here's an example of a FunSuite with ShouldMatchers mixed in:
-*/
 import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
-class ListSuite extends FunSuite with ShouldMatchers {
+class ListSuite extends FunSuite with Matchers {
 
   test("An empty list should be empty") {
     List() should be ('empty)
@@ -64,26 +53,17 @@ class ListSuite extends FunSuite with ShouldMatchers {
   }
 
   test("A non-empty list should not be empty") {
-    List(1, 2, 3) should not be ('empty)
-    List("fee", "fie", "foe", "fum") should not be ('empty)
+    List(1, 2, 3) should not be 'empty
+    List("fee", "fie", "foe", "fum") should not be 'empty
   }
 
   test("A list's length should equal the number of elements it contains") {
-    List() should have length (0)
-    List(1, 2) should have length (2)
-    List("fee", "fie", "foe", "fum") should have length (4)
+    List() should have length 0
+    List(1, 2) should have length 2
+    List("fee", "fie", "foe", "fum") should have length 4
   }
 }
 
-/*
-ScalaTest also supports the behavior-driven development style, in which you
-combine tests with text that specifies the behavior being tested. Here's
-an example whose text output when run looks like:
-
-A Map
-- should only contain keys and values that were added to it
-- should report its size as the number of key/value pairs it contains
-*/
 import org.scalatest.FunSpec
 import scala.collection.mutable.Stack
 
@@ -92,7 +72,7 @@ class ExampleSpec extends FunSpec {
   describe("A Stack") {
 
     it("should pop values in last-in-first-out order") {
-      val stack = new Stack[Int]
+      val stack = new mutable.Stack[Int]
       stack.push(1)
       stack.push(2)
       assert(stack.pop() === 2)
@@ -100,7 +80,7 @@ class ExampleSpec extends FunSpec {
     }
 
     it("should throw NoSuchElementException if an empty stack is popped") {
-      val emptyStack = new Stack[Int]
+      val emptyStack = new mutable.Stack[Int]
       intercept[NoSuchElementException] {
         emptyStack.pop()
       }
