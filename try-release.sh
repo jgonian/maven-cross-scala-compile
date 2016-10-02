224afdbb4002b2331a89fee3885dd6776b528fa7
Yannis Gonianakis <ygonianakis@ebay.com>
2016-10-02 20:16:02 +0200

    Add simple script for release

---
+++ try-release.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+RELEASE_PLUGIN="org.apache.maven.plugins:maven-release-plugin:2.5.3"
+ARGS="-Dcross-scala-compile -Dscala.compat.version=2.10 -Dscala.version=2.10.5"
+
+
+mvn clean install ${ARGS}
+mvn ${RELEASE_PLUGIN}:prepare --batch-mode -DreleaseVersion=0.8 -DdevelopmentVersion=0.9-SNAPSHOT -Darguments=${ARGS}
+mvn ${RELEASE_PLUGIN}:perform -Dgoals=deploy -Darguments=${ARGS}
+
