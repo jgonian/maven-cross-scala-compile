12e9de1c3ed2278666043d374599606b0271d7f7
Yannis Gonianakis <ygonianakis@ebay.com>
2016-10-02 20:34:09 +0200

    Remove batch mode

--- try-release.sh
+++ try-release.sh
@@ -3,8 +3,9 @@
 RELEASE_PLUGIN="org.apache.maven.plugins:maven-release-plugin:2.5.3"
 ARGS="-Dcross-scala-compile -Dscala.compat.version=2.10 -Dscala.version=2.10.5"
 
+#--batch-mode -DreleaseVersion=0.8 -DdevelopmentVersion=0.9-SNAPSHOT
 
 mvn clean install ${ARGS}
-mvn ${RELEASE_PLUGIN}:prepare --batch-mode -DreleaseVersion=0.8 -DdevelopmentVersion=0.9-SNAPSHOT -Darguments=${ARGS}
+mvn ${RELEASE_PLUGIN}:prepare -Darguments=${ARGS}
 mvn ${RELEASE_PLUGIN}:perform -Dgoals="deploy" -Darguments=${ARGS}
 
